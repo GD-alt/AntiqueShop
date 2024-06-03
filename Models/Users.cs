79a21cd94edfe82cfffcaac6ef3e3c152d1f313d
Danila <62538561+GD-alt@users.noreply.github.com>
2024-06-03 12:22:55 +0300

    Update Users.cs

--- Models/Users.cs
+++ Models/Users.cs
@@ -21,13 +21,13 @@ namespace AntiqueShop.Models
             this.Orders = new HashSet<Orders>();
         }
     
-        public int user_id { get; set; }
-        public string email { get; set; }
-        public string password { get; set; }
-        public string first_name { get; set; }
-        public string last_name { get; set; }
-        public int role_id { get; set; }
-        public string phone { get; set; }
+        // public int user_id { get; set; }
+        // public string email { get; set; }
+        // public string password { get; set; }
+        // public string first_name { get; set; }
+        // public string last_name { get; set; }
+        // public int role_id { get; set; }
+        // public string phone { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<CartItems> CartItems { get; set; }
